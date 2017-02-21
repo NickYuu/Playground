b7b703c038d69a4b50003f9eeb73aa0715021ed3
Tsung Han Yu <s3310304067@gms.nutc.edu.tw>
2017-02-21 14:01:22 +0800

    name

---
+++ Name.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var name = ["陳","林","黃","張","李","王","吳","劉","蔡","楊","許","鄭","謝","郭","洪","邱","曾","廖","賴","徐","周","葉","蘇","莊","江","呂","何","羅","高","蕭","潘","朱","簡","鍾","彭","游","詹"]
+var sex = ["先生", "小姐"]
+name[Int(arc4random_uniform(UInt32(name.count)))]
+
+sex[Int(arc4random_uniform(UInt32(sex.count)))]
