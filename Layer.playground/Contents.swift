1603d75c1af770df2758a5891138f71aea0f3638
NickYuu <s3310304067@gms.nutc.edu.tw>
2017-04-12 13:15:00 +0800

    4/12

---
+++ Layer.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+
+let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+btn.layer.borderColor = UIColor.blue.cgColor
+btn.layer.borderWidth = 1
+btn.layer.cornerRadius = 5
+btn.layer.masksToBounds = true
